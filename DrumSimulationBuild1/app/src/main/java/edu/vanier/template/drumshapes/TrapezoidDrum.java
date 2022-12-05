7aa569abac78bae589faebf1c0a74f8b4279bd54
Ryan2148317 <2148317@edu.vaniercollege.qc.ca>
2022-12-05 13:58:23 -0500

    Fixed trapezoid issue

--- DrumSimulationBuild1/app/src/main/java/edu/vanier/template/drumshapes/TrapezoidDrum.java
+++ DrumSimulationBuild1/app/src/main/java/edu/vanier/template/drumshapes/TrapezoidDrum.java
@@ -10,7 +10,6 @@ public class TrapezoidDrum extends Formable {
     
     private final int baseOne;
     private final int baseTwo;
-    private final int widthSide;
     private final int heightSide;
     private final int angle;
     
@@ -19,21 +18,20 @@ public class TrapezoidDrum extends Formable {
         this.baseTwo = baseTwo;
         this.heightSide = heightSide;
         this.angle = angle;
-        widthSide = baseOne > baseTwo ? baseOne : baseTwo;
     }
     /**
      * {@inheritDoc}
      */
     @Override
     public void generateMesh() {
-        Point[][] points = new Point[widthSide][heightSide];
+        Point[][] points = new Point[baseOne][heightSide];
         for (int j = 0; j < heightSide; j++){
-            for(int i = 0; i < widthSide; i++) {
+            for(int i = 0; i < baseOne; i++) {
                 if(j == 0 || j == heightSide - 1) {
                     //This puts two points on the edges and sets their onEdge value to true
                     points[i][j] = new Point(RADIUS, 0, NATURAL_MASS);
                     points[i][j].setOnEdge(true);
-                } else if(i == 0 || i == widthSide-1) {
+                } else if(i == 0 || i == baseOne-1) {
                     //This puts two points on the edges and sets their onEdge value to true
                     points[i][j] = new Point(RADIUS, 0, NATURAL_MASS);
                     points[i][j].setOnEdge(true);
@@ -42,10 +40,10 @@ public class TrapezoidDrum extends Formable {
                     points[i][j] = new Point(RADIUS, 0,NATURAL_MASS);
                     points[i][j].setOnEdge(false);
                 }
-                double level = baseTwo + j*(baseOne - baseTwo)/heightSide;
-                double step = i*level/widthSide;
-                double offset = 2*RADIUS*j/Math.tan(Math.toRadians(angle)) + step;
-                points[i][j].setup(2*RADIUS*i + offset, 2*RADIUS*j);
+                double staircase = 2*RADIUS*j/Math.tan(Math.toRadians(angle));
+                double changingLength = (baseOne-1) + j*(baseTwo-baseOne)/(heightSide-1);
+                double shift = 2*RADIUS*i*changingLength/(baseOne-1);
+                points[i][j].setup(staircase + shift, 2*RADIUS*j);
             }
         }
         this.mesh = points;
@@ -56,7 +54,7 @@ public class TrapezoidDrum extends Formable {
     @Override
     public void generateDrum() {
         ArrayList<Spring> springs = new ArrayList<>();
-        for(int i = 0; i < widthSide; i++){
+        for(int i = 0; i < baseOne; i++){
             for(int j = 0; j < heightSide; j++){
                 for(int[] pair : bindSpring(i, j)) {
                     try {
@@ -72,7 +70,7 @@ public class TrapezoidDrum extends Formable {
      */
     @Override
     protected int particleCount() {
-        return widthSide*heightSide;
+        return baseOne*heightSide;
     }
     /**
      * Creates distribution for parameter mass, decay, or spring constant in drum.
@@ -87,7 +85,7 @@ public class TrapezoidDrum extends Formable {
                 return distribution.getStops()[0];
             }
             case HORIZONTAL_GRADIENT -> {
-                return distribution.getStops()[0] + i*(distribution.getStops()[1] - distribution.getStops()[0])/(widthSide - 1);
+                return distribution.getStops()[0] + i*(distribution.getStops()[1] - distribution.getStops()[0])/(baseOne - 1);
             }
             case VERTICAL_GRADIENT -> {
                 return distribution.getStops()[0] + j*(distribution.getStops()[1] - distribution.getStops()[0])/(heightSide - 1);
@@ -95,11 +93,11 @@ public class TrapezoidDrum extends Formable {
             case RADIAL_GRADIENT -> {
                 int ringI;
                 int ringJ;
-                int ringTotal = widthSide < heightSide ? (int)Math.ceil(widthSide/2) : (int)Math.ceil(heightSide/2);
-                if(i < widthSide/2) {
+                int ringTotal = baseOne < heightSide ? (int)Math.ceil(baseOne/2) : (int)Math.ceil(heightSide/2);
+                if(i < baseOne/2) {
                     ringI = i;
                 } else {
-                    ringI = widthSide - i;
+                    ringI = baseOne - i;
                 }
                 if(j < heightSide/2) {
                     ringJ = j;
@@ -117,7 +115,7 @@ public class TrapezoidDrum extends Formable {
      */
     @Override
     public void generateMass() {
-        for(int i = 0; i < widthSide; i++) {
+        for(int i = 0; i < baseOne; i++) {
             for(int j = 0; j < heightSide; j++) {
                 mesh[i][j].setMass(distributeIndex(mass, i, j));
             }
